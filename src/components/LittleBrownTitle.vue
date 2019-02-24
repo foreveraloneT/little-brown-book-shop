52b5e5ffe14d7f8533bbb5741d4ad5eec9366e90
Sutee S <unlimited.tee@gmail.com>
2019-02-24 23:15:59 +0700

    animate on title shop

--- src/components/LittleBrownTitle.vue
+++ src/components/LittleBrownTitle.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>
-    <span class="text-green">Little Brown</span> <span class="text-black">Shop</span>
-  </h1>
+  <transition name="move" appear>
+    <h1>
+      <span class="text-green">Little Brown</span> <span class="text-black">Shop</span>
+    </h1>
+  </transition>
 </template>
 
 <script>
@@ -11,15 +13,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '@/scss/variable.scss';
-  h1 {
-    font-weight: bold;
-    margin-bottom: 5rem;
-    .text-green {
-      color: $primary-green;
-    }
-    .text-black {
-      color: $primary-monotone;
-    }
+@import '@/scss/variable.scss';
+h1 {
+  font-weight: bold;
+  margin-bottom: 5rem;
+  .text-green {
+    color: $primary-green;
   }
+  .text-black {
+    color: $primary-monotone;
+  }
+}
+.move-enter {
+  opacity: 0;
+  transform: translateX(-25%);
+}
+.move-enter-active {
+  transition: all .5s ease-in-out;
+}
 </style>
