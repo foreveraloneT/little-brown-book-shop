26a1c5731579844cf4bbee52c2e5c774ee8870ce
Sutee S <unlimited.tee@gmail.com>
2019-02-25 23:38:19 +0700

    update getter for cart

--- src/store/modules/cart/getters.js
+++ src/store/modules/cart/getters.js
@@ -2,5 +2,12 @@ import sum from 'lodash/sum'
 import values from 'lodash/values'
 
 export default {
-  itemCount: state => sum(values(state.items))
+  itemCount: state => sum(values(state.items)),
+  itemList: (state, getters, rootState, rootGetters) => {
+    const hashedBook = rootGetters['book/hashedData']
+    return Object.keys(state.items).map(id => ({
+      count: state.items[id],
+      ...hashedBook[id]
+    }))
+  }
 }
